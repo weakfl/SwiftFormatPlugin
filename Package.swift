0c35b0f64d4def25c5354baa5083d8e9bab73d9b
weak <weak@fraglab.at>
2025-11-12 13:09:51 +0100

    update v0.58.6

--- Package.swift
+++ Package.swift
@@ -30,8 +30,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "swiftformat",
-            url: "https://github.com/nicklockwood/SwiftFormat/releases/download/0.58.5/swiftformat.artifactbundle.zip",
-            checksum: "f0fc3dd92c44e0ff3b856042865559c5aec175419661139da106768fbace3158"
+            url: "https://github.com/nicklockwood/SwiftFormat/releases/download/0.58.6/swiftformat.artifactbundle.zip",
+            checksum: "d2ee571b3f15c173b1789b82b9fcf1e799cff66de0ae9f6839bd35aa8e9b9608"
         )
     ]
 )
